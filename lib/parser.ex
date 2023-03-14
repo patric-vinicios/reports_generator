805d2d2455509c0b706335bf29e1a1d42f109c70
Patric Vinicios <psdev7@gmail.com>
2023-03-14 11:40:08 -0300

    first commit

---
+++ lib/parser.ex
@@ -0,0 +1,14 @@
+defmodule Parser do
+  def call(filename) do
+    "./reports/#{filename}.csv"
+    |> File.stream!()
+    |> Stream.map(&parse_line/1)
+  end
+
+  defp parse_line(line) do
+    line
+    |> String.trim()
+    |> String.split(",")
+    |> List.update_at(2, &String.to_integer/1)
+  end
+end
